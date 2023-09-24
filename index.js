bba57625ae787e3404ddcdc9f29f0fbc980181d0
Jerry Kipchirchir <jerrytarus@gmail.com>
2023-09-24 23:30:19 +0300

    Completed Control Flow Lab Assignment

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result; 
+  if (distance <= 400){
+    result = "This one is on me!";
+  } else if (distance >= 2000 && distance <= 2500){
+    result = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500){
+    result = "No can do.";
+  }
+  return result;
+  }
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+// function switchOnCharmFromTip(){
+//   // Write your code here!
+//     let tip = "generous";
+//     switch (tip) {
+//       case 'generous':
+//         return ;
+//         break;
+//       case "not as generous":
+//         return "Thank you.";
+//         break;
+//       default:
+//         return "Bye.";
+//     }
+    
+//   }
+
+function switchOnCharmFromTip(tip) {
+  let tip = "generous";
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you";
+    default:
+      return "Bye.";
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
